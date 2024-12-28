6b4aad710eac4293404773ac78c1226383ec373b
Derrick-Projects <derrick.otieno@stud.th-deg.de>
2024-12-28 14:23:21 +0100

    Delete docs directory

--- docs/trait.impl/clap_builder/derive/trait.FromArgMatches.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["quotes",[["impl FromArgMatches for <a class=\"struct\" href=\"quotes/struct.Cli.html\" title=\"struct quotes::Cli\">Cli</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[129]}
--- docs/trait.impl/core/marker/trait.Freeze.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["quotes",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/marker/trait.Freeze.html\" title=\"trait core::marker::Freeze\">Freeze</a> for <a class=\"struct\" href=\"quotes/struct.Cli.html\" title=\"struct quotes::Cli\">Cli</a>",1,["quotes::Cli"]]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[271]}
