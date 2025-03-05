39d1ff14a4897e599c43136a6bebf893c3fbc0bf
Derrick-Projects <derrick.otieno@stud.th-deg.de>
2025-03-05 10:48:24 +0100

    Version 4

---
+++ target/aarch64-unknown-linux-gnu/release/deps/form_urlencoded-1bfd8966774fa823.d
@@ -0,0 +1,7 @@
+C:\Users\H.P\Desktop\Quote Generator\quotes\target\aarch64-unknown-linux-gnu\release\deps\libform_urlencoded-1bfd8966774fa823.rmeta: C:\Users\H.P\.cargo\registry\src\index.crates.io-6f17d22bba15001f\form_urlencoded-1.2.1\src\lib.rs
+
+C:\Users\H.P\Desktop\Quote Generator\quotes\target\aarch64-unknown-linux-gnu\release\deps\libform_urlencoded-1bfd8966774fa823.rlib: C:\Users\H.P\.cargo\registry\src\index.crates.io-6f17d22bba15001f\form_urlencoded-1.2.1\src\lib.rs
+
+C:\Users\H.P\Desktop\Quote Generator\quotes\target\aarch64-unknown-linux-gnu\release\deps\form_urlencoded-1bfd8966774fa823.d: C:\Users\H.P\.cargo\registry\src\index.crates.io-6f17d22bba15001f\form_urlencoded-1.2.1\src\lib.rs
+
+C:\Users\H.P\.cargo\registry\src\index.crates.io-6f17d22bba15001f\form_urlencoded-1.2.1\src\lib.rs:
